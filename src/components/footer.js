6ebc001fae3220a3c525f6a4d154fc2e523480e7
Zbigniew Downar <zbyszrom@Zbigniews-iMac.local>
2019-12-13 18:27:49 +0100

    start building

---
+++ src/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Footer = ({children}) => {
+    
+   
+
+    
+    return (
+        <footer >  
+            
+            <p>Created by zbyszrom, © 2019</p>   
+        
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'gatsby'
+
+const Nav = () => {
+
+  
+    return(
+
+    <nav>
+      
+        
+    </nav>
+)}
+
+export default Nav
